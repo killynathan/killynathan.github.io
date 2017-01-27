e9a60acb7848af642cf148f92358c249415360be
nathan <nathankou12@gmail.com>
2017-01-27 02:55:41 -0600

    jumbotron

---
+++ script.js
@@ -0,0 +1,50 @@
+var jumbotron1 = document.getElementById("jumbotron1");
+var jumbotron2 = document.getElementById("jumbotron2");
+var jumbotron3 = document.getElementById("jumbotron3");
+var logo = document.getElementById("logo");
+
+var setIntervalId;
+var index = 2;
+var counter = 0;
+var nextJumbotron;
+var currentJumbotron = jumbotron1;
+
+// JUMBOTRON --- JUMBOTRON --- JUMBOTRON --- JUMBOTRON --- JUMBOTRON 
+
+function startAnimatingJumbotron() {
+	setIntervalId = setInterval(animateJumbotron, 100);
+}
+
+function getJumbotron(index) {
+	switch(index) {
+		case 1: return jumbotron1;
+		case 2: return jumbotron2;
+		case 3: return jumbotron3;
+	}
+}
+
+function animateJumbotron() {
+
+	if (counter === 30) {
+		nextJumbotron = getJumbotron(index);
+		nextJumbotron.classList.add("fadeIn");
+		//nextJumbotron.classList.add("animateJumbotron");
+
+		index++;
+		if (index == 4) index = 1;
+	}
+
+	else if (counter === 50) {
+		nextJumbotron.classList.add("animateJumbotron");
+		currentJumbotron.classList.remove("animateJumbotron");
+
+		nextJumbotron.classList.remove("fadeIn");
+		
+		currentJumbotron = nextJumbotron;
+	}
+
+	counter++;
+	if (counter === 51) counter = 0;
+}
+
+startAnimatingJumbotron();
