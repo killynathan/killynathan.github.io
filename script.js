6a492e36dfdfefbcd0a4b9d53b2dee07bd160a31
nathan <nathankou12@gmail.com>
2017-04-01 03:18:00 -0500

    ball

--- script.js
+++ script.js
@@ -1,65 +1,32 @@
-var jumbotron1 = document.getElementById("jumbotron1");
-var jumbotron2 = document.getElementById("jumbotron2");
-var jumbotron3 = document.getElementById("jumbotron3");
-var jumbotron4 = document.getElementById("jumbotron4");
+var ball = document.getElementById('ball');
+var menuWeb = document.getElementById('menuWeb');
+var menuAbout = document.getElementById('menuAbout');
+var menuBlog = document.getElementById('menuBlog');
+var about = document.getElementById('about');
+var web = document.getElementById('web');
 
-var workGameButton = document.getElementById("workGame");
-var workPomodoroButton = document.getElementById("workPomodoro");
-var workQuoteButton = document.getElementById("workQuote");
-var workSnakeButton = document.getElementById("workSnake");
+menuWeb.onclick = function() {
+	ball.style.left = '9px';
+	ball.style.backgroundColor = '#FF69B4';
+	about.style.right = '110%';
+	web.style.left = '0%';
+};
 
-var setIntervalId;
-var index = 2;
-var counter = 0;
-var nextJumbotron;
-var currentJumbotron = jumbotron1;
+menuAbout.onclick = function() {
+	ball.style.left = '92px';
+	ball.style.backgroundColor = '#39FF14';
+	about.style.right = '0%';
+	web.style.left =  '110%';
+};
 
-// JUMBOTRON --- JUMBOTRON --- JUMBOTRON --- JUMBOTRON --- JUMBOTRON --- JUMBOTRON 
+menuBlog.onclick = function() {
+	ball.style.left = '177px'
+};
 
-function startAnimatingJumbotron() {
-	setIntervalId = setInterval(animateJumbotron, 100);
-}
 
-function getJumbotron(index) {
-	switch(index) {
-		case 1: return jumbotron1;
-		case 2: return jumbotron2;
-		case 3: return jumbotron3;
-		case 4: return jumbotron4;
-	}
-}
 
-function animateJumbotron() {
 
-	if (counter === 30) {
-		nextJumbotron = getJumbotron(index);
-		nextJumbotron.classList.add("fadeIn");
-		//nextJumbotron.classList.add("animateJumbotron");
 
-		index++;
-		if (index == 5) index = 1;
-	}
-
-	else if (counter === 50) {
-		nextJumbotron.classList.add("animateJumbotron");
-		currentJumbotron.classList.remove("animateJumbotron");
-
-		nextJumbotron.classList.remove("fadeIn");
-		
-		currentJumbotron = nextJumbotron;
-	}
-
-	counter++;
-	if (counter === 51) counter = 0;
-}
-
-startAnimatingJumbotron();
-
-// WORK --- WORK --- WORK --- WORK --- WORK --- WORK --- WORK --- WORK --- WORK  
-
-function displayWorkGame() {
-	
-}
 
 
 
