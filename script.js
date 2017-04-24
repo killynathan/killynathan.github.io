b99fb4cc0d9703fbd8d4a677a16466f9738c4a0c
nathan <nathankou12@gmail.com>
2017-04-24 04:10:41 -0500

    cool

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var menuAbout = document.getElementById('menuAbout');
 var menuBlog = document.getElementById('menuBlog');
 var about = document.getElementById('about');
 var web = document.getElementById('web');
+var menuItems = document.getElementsByClassName('menuItem');
 
 menuWeb.onclick = function() {
 	ball.style.left = '9px';
@@ -11,6 +12,7 @@ menuWeb.onclick = function() {
 	about.style.opacity = '0';
 	web.style.opacity = '100';
 	about.style.zIndex = '-1';
+	handleMenuClick(menuWeb);
 };
 
 menuAbout.onclick = function() {
@@ -19,11 +21,20 @@ menuAbout.onclick = function() {
 	about.style.opacity = '100';
 	web.style.opacity=  '0';
 	about.style.zIndex = '2';
+	handleMenuClick(menuAbout);
 };
 
 menuBlog.onclick = function() {
-	ball.style.left = '177px'
+	ball.style.left = '177px';
+	handleMenuClick(menuBlog);
 };
+
+function handleMenuClick(menuItem) {
+	for (i=0; i < menuItems.length; i++) {
+		menuItems[i].classList.remove('active');
+	}
+	menuItem.classList.add('active');
+}
 
 
 
