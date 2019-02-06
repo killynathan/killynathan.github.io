65bc1ae11d212c9ee74654fd4fa2e20ee66d49e1
nathan kou <nathankou12@gmail.com>
2019-02-06 02:37:39 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Project from "./components/Project";
+
+const projects = [
+  {
+    title: "Personality.ai",
+    desription: "Lookup the personality of a social media profile"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -18,7 +26,8 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
+          <Project />
         </header>
       </div>
     );
---
+++ src/components/Project.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Project = () => (
+  <div className="project">
+    <div className="project-content">
+      <p className="project-title">title</p>
+      <image />
+      <p className="project-description">description</p>
+    </div>
+    <p className="text-button">GITHUB</p>
+    <p className="text-button">LINK</p>
+  </div>
+);
+
+export default Project;
